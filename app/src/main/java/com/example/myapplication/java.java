bfb7f6ed9c811d82d266a959ec10e8f1857106e9
akshay saini <akshaysaini2005@gmail.com>
2023-04-04 16:31:08 +0530

    in java creating button by b2 id for go to next activity_sum.xml

--- app/src/main/java/com/example/myapplication/java.java
+++ app/src/main/java/com/example/myapplication/java.java
@@ -13,6 +13,8 @@ public class java extends AppCompatActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java);
+        Button button1 = findViewById(R.id.b2);
+        button1.setOnClickListener(this);
 
 
     }
@@ -20,5 +22,15 @@ public class java extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
+        switch (v.getId()){
+            case R.id.b2:
+                Toast.makeText(this,"JAVA", Toast.LENGTH_LONG).show();
+                Intent b = new Intent(java.this, Sum.class);
+                startActivity(b);
+                break;}
 
-}}
+
+
+
+        }
+    }
